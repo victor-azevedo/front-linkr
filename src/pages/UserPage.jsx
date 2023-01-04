6e221ba5ec1f4d84b3cc6c88a6bfafc3b4f09c8b
Yuri Desideri <yuridesideri12@gmail.com>
2023-01-04 16:32:40 -0300

    feat: UserPage request to api

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,20 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import Header from "../components/Header.jsx";
+import Trending from "../components/Trending";
 import { useAuth } from "../hooks/useAuth.jsx";
 
+export default function UserPage(props) {
+    //TODO const [auth] = useAuth();
+    //TODO authenticated route? const {token} = auth;
+    // if (!auth)
+    const { id: userId } = useParams();
+    const [cards, setCards] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const URL = "";
 
-export default function UserPage(props){
-
-    const [auth] = useAuth();
-
-    //TODO if (!auth) 
-
-    const {username, pictureUrl} = auth;
+    useEffect(() => {
+        setLoading(true);
+        axios
+            .get(URL + `/user/${userId}`)
+            .then(({ data: userCards }) => {
+                setLoading(false);
+                setCards(userCards);
+            })
+            .catch((err) => {
+                setLoading(false);
+                console.log(err);
+            });
+    });
 
     return (
         <StyledUserPage>
             <Header />
-
+            <div>
+                <Trending />
+            </div>
+            {loading ? <h1>Loading</h1> : cards}
         </StyledUserPage>
     );
 }
@@ -23,4 +44,4 @@ const StyledUserPage = styled.div`
     width: 100%;
     min-height: 100vh;
     background-color: #333333;
-`
+`;
