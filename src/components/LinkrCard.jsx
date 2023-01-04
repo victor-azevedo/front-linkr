add00c39d49fa8bbe146292216ae6b09b1d2174d
Victor Azevedo <josevcazevedo@gmail.com>
2023-01-04 20:58:51 -0300

    feat: LinkrCard design

--- src/components/LinkrCard.jsx
+++ src/components/LinkrCard.jsx
@@ -7,10 +7,25 @@ export default function LinkrCard({ pictureUrl, link }) {
       <UserPicture pictureUrl={pictureUrl} />
       <div className="link-data">
         <Username>Juvenal Juvêncio</Username>
-        <LinkText>
+        <Text>
           Muito maneiro esse tutorial de Material UI com React, deem uma olhada!
-        </LinkText>
-        <Link>{link}</Link>
+        </Text>
+        <Link>
+          <LinkTexts>
+            <LinkTitle>
+              Como aplicar o Material UI em um projeto React
+            </LinkTitle>
+            <LinkDescription>
+              Hey! I have moved this tutorial to my personal blog. Same content,
+              new location. Sorry about making you click through to another
+              page.
+            </LinkDescription>
+            <LinkUrl>{link}</LinkUrl>
+          </LinkTexts>
+          <LinkImage>
+            <img src={pictureUrl} alt="" />
+          </LinkImage>
+        </Link>
       </div>
     </LinkCardStyle>
   );
@@ -38,16 +53,51 @@ const Username = styled.p`
   font-weight: 400;
 `;
 
-const LinkText = styled.p`
+const Text = styled.p`
   color: #b7b7b7;
   font-size: 17px;
   line-height: 20px;
   font-weight: 400;
+  margin-top: 10px;
 `;
 
 const Link = styled.div`
-  color: #cecece;
+  display: flex;
+  align-items: stretch;
+  border: 1px solid #4d4d4d;
+  border-radius: 11px;
+  margin-top: 10px;
+`;
+
+const LinkTexts = styled.div`
+  padding: 24px;
+`;
+
+const LinkTitle = styled.p`
   font-size: 16px;
   line-height: 19px;
   font-weight: 400;
+`;
+const LinkDescription = styled.p`
+  color: #9b9595;
+  font-size: 11px;
+  line-height: 13px;
+  font-weight: 400;
+  margin-top: 10px;
+`;
+const LinkUrl = styled.p`
+  font-size: 11px;
+  line-height: 13.2px;
+  font-weight: 400;
+  margin-top: 10px;
+`;
+const LinkImage = styled.div`
+  flex-basis: 155px;
+  height: 100%;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 0 11px 11px 0;
+  }
 `;
