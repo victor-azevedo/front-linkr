529aff2bb0e7b1f04594a5d3a59aade4e9edef57
Yuri Desideri <yuridesideri12@gmail.com>
2023-01-04 15:52:56 -0300

    feat: UserPage styles and authentication data

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,12 +1,26 @@
+import styled from "styled-components";
+import Header from "../components/Header.jsx";
+import { useAuth } from "../hooks/useAuth.jsx";
 
 
 export default function UserPage(props){
 
-    
+    const [auth] = useAuth();
+
+    //TODO if (!auth) 
+
+    const {username, pictureUrl} = auth;
 
     return (
-        <>
-            <h1>Hello UserPage</h1> 
-        </>
+        <StyledUserPage>
+            <Header />
+
+        </StyledUserPage>
     );
 }
+
+const StyledUserPage = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    background-color: #333333;
+`
