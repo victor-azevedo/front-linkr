6eb3fa0d3831bf7b71d6ebc67f7f3fdb31345b7b
Yuri Desideri <yuridesideri12@gmail.com>
2023-01-05 21:34:47 -0300

    feat: card removal completed

--- src/components/LinkrCard.jsx
+++ src/components/LinkrCard.jsx
@@ -3,16 +3,19 @@ import { useAuth } from "../hooks/useAuth";
 import UserPicture from "./UserPicture";
 import RemoveIcon from "../assets/RemoveIcon.svg";
 import EditIcon from "../assets/EditIcon.svg";
+import { useState } from "react";
+import axios from "axios";
 
-export default function LinkrCard({ username, userPictureUrl, link, text, linkMetadata }) {
+export default function LinkrCard({ id, username, userPictureUrl, link, text, linkMetadata }) {
     const [auth] = useAuth();
-
-    function handleCardEdit(e) {
-        e.preventDefault();
-    }
+    const [modalConfirmation, setModalConfirmation] = useState(false);
 
     function handleCardRemoval(e) {
         e.preventDefault();
+        axios
+            .delete(`/linkr/${id}`)
+            .then((res) => alert("Success"))
+            .catch((error) => alert("Error in removal"));
     }
 
     return (
@@ -22,11 +25,11 @@ export default function LinkrCard({ username, userPictureUrl, link, text, linkMe
                 {
                     /*auth?.username*/ "didi" === username && (
                         <EditionAndDeletion>
-                            <img src={EditIcon} alt="edit linkr icon" onClick={handleCardEdit} />
+                            <img src={EditIcon} alt="edit linkr icon" onClick={(e) => {}} />
                             <img
                                 src={RemoveIcon}
                                 alt="remove linkr icon"
-                                onClick={handleCardRemoval}
+                                onClick={(e) => setModalConfirmation(true)}
                             />
                         </EditionAndDeletion>
                     )
@@ -44,6 +47,24 @@ export default function LinkrCard({ username, userPictureUrl, link, text, linkMe
                     </LinkImage>
                 </Link>
             </div>
+            {modalConfirmation && (
+                <ModalConfirmationScreen>
+                    <div className="confirmation-box">
+                        <h2>Are you sure you want to delete this post?</h2>
+                        <div className="confirmation-buttons">
+                            <button
+                                onClick={(e) => setModalConfirmation(false)}
+                                className="return-button"
+                            >
+                                No, go back
+                            </button>
+                            <button onClick={handleCardRemoval} className="delete-button">
+                                Yes, delete it
+                            </button>
+                        </div>
+                    </div>
+                </ModalConfirmationScreen>
+            )}
         </LinkCardStyle>
     );
 }
@@ -130,4 +151,55 @@ const EditionAndDeletion = styled.div`
     right: 0;
     width: 40px;
     height: 20px;
+`;
+
+const ModalConfirmationScreen = styled.div`
+    position: fixed;
+    z-index: 3;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(255, 255, 255, 0.9);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .confirmation-box {
+        width: 597px;
+        height: 262px;
+        background-color: #333333;
+        border-radius: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        h2 {
+            width: 340px;
+            font-size: 34px;
+            font-weight: 700;
+            text-align: center;
+            font-family: "Lato", sans-serif;
+        }
+        .confirmation-buttons {
+            display: flex;
+            width: 60%;
+            justify-content: space-around;
+            button {
+                border-radius: 5px;
+                width: 134px;
+                height: 37px;
+                border: 0;
+                font-size: 18px;
+                font-weight: 700;
+            }
+            .return-button {
+                background-color: #ffffff;
+                color: #1877f2;
+            }
+            .delete-button {
+                background-color: #1877f2;
+                color: white;
+            }
+        }
+    }
 `;
