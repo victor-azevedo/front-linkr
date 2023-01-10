6a9fc913e1793070a9b68fc0461be510fa4bca3f
Luhenrique09 <luhenrique1100@gmail.com>
2023-01-10 11:54:09 -0300

    feat: signIn and signUp responsiveness

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -106,6 +106,12 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   height: 100vh;
+
+  @media (max-width: 600px){
+    display: initial;
+    height: 100%;
+    
+}
 `;
 const SideLeft = styled.div`
   width: 100%;
@@ -131,6 +137,21 @@ const SideLeft = styled.div`
     font-size: 43px;
     line-height: 64px;
   }
+
+  @media (max-width: 600px){
+    height: 175px;
+    align-items: center;
+    padding-left: 0;
+    h1{
+      font-size: 76px;
+      line-height: 84px;
+    }
+
+    h2{
+      font-size: 23px;
+      line-height: 34px;
+    }
+}
 `;
 const SideRight = styled.div`
   background-color: #333333;
@@ -158,11 +179,16 @@ const SideRight = styled.div`
   }
 
   input {
-    width: 429px;
+    width: 100%;
     height: 65px;
     border-radius: 6px;
     border: none;
     margin-bottom: 13px;
+
+    @media (max-width: 600px){
+     
+     height: 55px;
+    }
   }
 
   button {
@@ -177,5 +203,20 @@ const SideRight = styled.div`
     border-radius: 6px;
     border: none;
     margin-bottom: 14px;
+
+    @media (max-width: 600px){
+      
+      height: 55px;
+    }
+  }
+
+  @media (max-width: 600px){
+    height: 100vh;
+    flex-direction: initial;
+    padding-top: 40px;
+
+    padding-left: 0;
+    padding-right: 0;
   }
 `;
+
