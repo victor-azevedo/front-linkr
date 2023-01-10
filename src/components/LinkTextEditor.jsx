f5e0e477840c916378732d7e901e075ceef20a18
Victor Azevedo <josevcazevedo@gmail.com>
2023-01-10 16:41:00 -0300

    refactor: add TextEdit and ModalDelete components

---
+++ src/components/LinkTextEditor.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useRef, useState } from "react";
+
+import axios from "axios";
+import styled from "styled-components";
+import { useUserData } from "../hooks/useUserData";
+
+export default function LinkTextEditor({
+  setEditTextInput,
+  isTextEditable,
+  setIsTextEditable,
+  editTextInput,
+  text,
+  id,
+}) {
+  const { userData } = useUserData();
+  const [loadingEdition, setLoadingEdition] = useState(false);
+  const [editAPIAccepted, setEditAPIAccepted] = useState(false);
+
+  const inputRef = useRef(null);
+
+  function handleKeyEvent(e) {
+    if (e.keyCode === 27) {
+      //ESC Key
+      setIsTextEditable(false);
+    }
+    if (e.keyCode === 13) {
+      //ENTER Key
+      if (editTextInput === text) {
+        setIsTextEditable(false);
+        return;
+      }
+      setLoadingEdition(true);
+      axios
+        .put(
+          `${process.env.REACT_APP_BASE_URL}/linkrs/edit/${id}`,
+          { updatedText: editTextInput },
+          userData.requestConfig
+        )
+        .then((res) => {
+          text = editTextInput;
+          setEditAPIAccepted(true);
+          setLoadingEdition(false);
+          setIsTextEditable(false);
+        })
+        .catch((err) => {
+          alert("Couldn't complete request");
+          setLoadingEdition(false);
+        });
+    }
+  }
+
+  useEffect(() => {
+    const { current } = inputRef;
+    if (isTextEditable) {
+      current.focus();
+    } else {
+      setEditTextInput(editAPIAccepted ? editTextInput : text);
+    }
+  }, [isTextEditable]);
+
+  return (
+    <TextEditor
+      type="text"
+      onKeyDown={handleKeyEvent}
+      ref={inputRef}
+      disabled={loadingEdition}
+      value={editTextInput}
+      onChange={(e) => {
+        setEditTextInput(e.target.value);
+      }}
+    />
+  );
+}
+
+const TextEditor = styled.input`
+  width: 100%;
+  font-size: 17px;
+  line-height: 20px;
+  font-weight: 400;
+  margin-top: 10px;
+  word-break: break-all;
+  padding: 5px;
+  border-radius: 7px;
+  color: #4c4c4c;
+  font-family: "Lato", sans-serif;
+  border: 0;
+  &:focus {
+    outline: 0;
+  }
+`;
--- src/components/LinkrCard.jsx
+++ src/components/LinkrCard.jsx
@@ -1,16 +1,17 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
 import styled from "styled-components";
-
-import { useAuth } from "../hooks/useAuth";
 
 import EditIcon from "../assets/EditIcon.svg";
 import BoxLikes from "./BoxLikes";
 import RemoveIcon from "../assets/RemoveIcon.svg";
 import UserPicture from "./UserPicture";
+
+import ModalConfirmationDelete from "./ModalConfirmationDelete";
+import LinkTextEditor from "./LinkTextEditor";
 import { useUserData } from "../hooks/useUserData";
-import { useNavigate } from "react-router-dom";
 
 export default function LinkrCard({
   id,
@@ -20,7 +21,7 @@ export default function LinkrCard({
   text,
   linkMetadata,
   likes,
-  userId
+  userId,
 }) {
   const { userData } = useUserData();
   const [modalConfirmation, setModalConfirmation] = useState(false);
@@ -28,9 +29,6 @@ export default function LinkrCard({
 
   const [isTextEditable, setIsTextEditable] = useState(false);
   const [editTextInput, setEditTextInput] = useState(text);
-  const [loadingEdition, setLoadingEdition] = useState(false);
-  const [editAPIAccepted, setEditAPIAccepted] = useState(false);
-  const inputRef = useRef(null);
 
   const navigate = useNavigate();
 
@@ -56,46 +54,6 @@ export default function LinkrCard({
       });
   }
 
-  function handleKeyEvent(e) {
-    if (e.keyCode === 27) {
-      //ESC Key
-      setIsTextEditable(false);
-    }
-    if (e.keyCode === 13) {
-      //ENTER Key
-      if (editTextInput === text) {
-        setIsTextEditable(false);
-        return;
-      }
-      setLoadingEdition(true);
-      axios
-        .put(
-          `${process.env.REACT_APP_BASE_URL}/linkrs/edit/${id}`,
-          { updatedText: editTextInput },
-          userData.requestConfig
-        )
-        .then((res) => {
-          text = editTextInput;
-          setEditAPIAccepted(true);
-          setLoadingEdition(false);
-          setIsTextEditable(false);
-        })
-        .catch((err) => {
-          alert("Couldn't complete request");
-          setLoadingEdition(false);
-        });
-    }
-  }
-
-  useEffect(() => {
-    const { current } = inputRef;
-    if (isTextEditable) {
-      current.focus();
-    } else {
-      setEditTextInput(editAPIAccepted ? editTextInput : text);
-    }
-  }, [isTextEditable]);
-
   return (
     <LinkCardStyle>
       <div>
@@ -117,17 +75,17 @@ export default function LinkrCard({
             />
           </EditionAndDeletion>
         )}
-        <Username onClick={() => navigate(`/user/${userId}`)}>{username}</Username>
+        <Username onClick={() => navigate(`/user/${userId}`)}>
+          {username}
+        </Username>
         {isTextEditable ? (
-          <TextEditor
-            type="text"
-            onKeyDown={handleKeyEvent}
-            ref={inputRef}
-            disabled={loadingEdition}
-            value={editTextInput}
-            onChange={(e) => {
-              setEditTextInput(e.target.value);
-            }}
+          <LinkTextEditor
+            id={id}
+            setEditTextInput={setEditTextInput}
+            isTextEditable={isTextEditable}
+            setIsTextEditable={setIsTextEditable}
+            editTextInput={editTextInput}
+            text={text}
           />
         ) : (
           <Text>{editTextInput}</Text>
@@ -144,23 +102,11 @@ export default function LinkrCard({
         </Link>
       </div>
       {modalConfirmation && (
-        <ModalConfirmationScreen>
-          <div className="confirmation-box">
-            <h2>Are you sure you want to delete this post?</h2>
-            <div className="confirmation-buttons">
-              <button
-                onClick={(e) => setModalConfirmation(false)}
-                className="return-button"
-              >
-                No, go back
-              </button>
-              <button onClick={handleCardRemoval} className="delete-button">
-                Yes, delete it
-              </button>
-            </div>
-          </div>
-          {modalLoading && <p className="loading-p">Loading...</p>}
-        </ModalConfirmationScreen>
+        <ModalConfirmationDelete
+          modalLoading={modalLoading}
+          setModalConfirmation={setModalConfirmation}
+          handleCardRemoval={handleCardRemoval}
+        />
       )}
     </LinkCardStyle>
   );
@@ -251,80 +197,4 @@ const EditionAndDeletion = styled.div`
   right: 0;
   width: 40px;
   height: 20px;
-`;
-
-const TextEditor = styled.input`
-  width: 100%;
-  font-size: 17px;
-  line-height: 20px;
-  font-weight: 400;
-  margin-top: 10px;
-  word-break: break-all;
-  padding: 5px;
-  border-radius: 7px;
-  color: #4c4c4c;
-  font-family: "Lato", sans-serif;
-  border: 0;
-  &:focus {
-    outline: 0;
-  }
-`;
-
-const ModalConfirmationScreen = styled.div`
-  position: fixed;
-  z-index: 3;
-  top: 0;
-  right: 0;
-  width: 100%;
-  height: 100vh;
-  background-color: rgba(255, 255, 255, 0.9);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  .confirmation-box {
-    width: 597px;
-    height: 262px;
-    background-color: #333333;
-    border-radius: 50px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    h2 {
-      width: 340px;
-      font-size: 34px;
-      font-weight: 700;
-      text-align: center;
-      font-family: "Lato", sans-serif;
-      color: white;
-    }
-    .confirmation-buttons {
-      display: flex;
-      width: 60%;
-      justify-content: space-around;
-      button {
-        border-radius: 5px;
-        width: 134px;
-        height: 37px;
-        border: 0;
-        font-size: 18px;
-        font-weight: 700;
-      }
-      .return-button {
-        background-color: #ffffff;
-        color: #1877f2;
-      }
-      .delete-button {
-        background-color: #1877f2;
-        color: white;
-      }
-    }
-  }
-  .loading-p {
-    margin-top: 20px;
-    color: black;
-    font-family: "Oswald", sans-serif;
-    font-size: 28px;
-  }
 `;
