53d0e7285be69e683992230a85d398e44a664579
Victor Azevedo <josevcazevedo@gmail.com>
2023-01-14 00:01:45 -0300

    feat: signIn and signUp pages responsive

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -59,61 +59,60 @@ export default function SignIn(props) {
   }
 
   return (
-    <>
-      <Container>
-        <SideLeft>
-          <h1>Linkr</h1>
-          <h2>
-            save, share and discover<br></br>
-            the best links on the web
-          </h2>
-        </SideLeft>
+    <Container>
+      <SideLeft>
+        <h1>Linkr</h1>
+        <h2>
+          save, share and discover<br></br>
+          the best links on the web
+        </h2>
+      </SideLeft>
 
-        <SideRight>
-          <form onSubmit={handleSubmit}>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              disabled={isLoading}
-              placeholder="e-mail"
-              name="email"
-              type="email"
-              required
-            ></input>
+      <SideRight>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            disabled={isLoading}
+            placeholder="e-mail"
+            name="email"
+            type="email"
+            required
+          ></input>
 
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              disabled={isLoading}
-              placeholder="password"
-              name="password"
-              type="password"
-              required
-            ></input>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            disabled={isLoading}
+            placeholder="password"
+            name="password"
+            type="password"
+            required
+          ></input>
 
-            <button type="submit" disabled={isLoading}>
-              Log in
-            </button>
-            <Link to="/sign-up">First time? Create an account!</Link>
-          </form>
-        </SideRight>
-      </Container>
-    </>
+          <button type="submit" disabled={isLoading}>
+            Log in
+          </button>
+          <Link to="/sign-up">First time? Create an account!</Link>
+        </form>
+      </SideRight>
+    </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
+  width: 100vw;
   height: 100vh;
 
   @media (max-width: 600px) {
-    display: initial;
-    height: 100%;
+    flex-direction: column;
   }
 `;
 const SideLeft = styled.div`
-  width: 100%;
+  width: 60%;
+  height: 100%;
   background-color: #151515;
 
   color: white;
@@ -122,7 +121,8 @@ const SideLeft = styled.div`
   flex-direction: column;
   justify-content: center;
 
-  padding-left: 144px;
+  padding-left: 5%;
+  padding-right: 15%;
 
   h1 {
     font-size: 106px;
@@ -137,34 +137,64 @@ const SideLeft = styled.div`
     line-height: 64px;
   }
 
+  @media (max-width: 840px) {
+    width: 55%;
+    padding-left: 4%;
+    padding-right: 12%;
+    h1 {
+      font-size: 86px;
+      line-height: 92px;
+    }
+
+    h2 {
+      font-size: 32px;
+      line-height: 42px;
+    }
+  }
+
   @media (max-width: 600px) {
-    height: 175px;
-    align-items: center;
-    padding-left: 0;
+    width: 100%;
+    height: 40%;
+    text-align: center;
+    padding: 20px 10px;
     h1 {
       font-size: 76px;
       line-height: 84px;
     }
 
     h2 {
-      font-size: 23px;
-      line-height: 34px;
+      font-size: 22px;
+      line-height: 30px;
+    }
+  }
+  @media (max-width: 360px) {
+    height: 35%;
+    h1 {
+      font-size: 54px;
+      line-height: 60px;
+    }
+
+    h2 {
+      font-size: 18px;
+      line-height: 20px;
     }
   }
 `;
 const SideRight = styled.div`
+  width: 40%;
+  height: 100%;
   background-color: #333333;
 
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  align-items: center;
 
-  padding-left: 52px;
-  padding-right: 54px;
+  padding: 0 5%;
 
   form {
     display: flex;
     flex-direction: column;
+    align-items: center;
+    width: 100%;
     a {
       color: white;
       font-family: "Lato", sans-serif;
@@ -173,7 +203,7 @@ const SideRight = styled.div`
       font-size: 20px;
       line-height: 24px;
       text-decoration-line: underline;
-      margin: 0 auto;
+      text-align: center;
     }
   }
 
@@ -183,15 +213,11 @@ const SideRight = styled.div`
     border-radius: 6px;
     border: none;
     margin-bottom: 13px;
-
-    @media (max-width: 600px) {
-      height: 55px;
-    }
   }
 
   button {
     color: white;
-    width: 433px;
+    width: 100%;
     height: 65px;
     font-family: "Oswald", sans-serif;
     font-weight: 700;
@@ -201,19 +227,43 @@ const SideRight = styled.div`
     border-radius: 6px;
     border: none;
     margin-bottom: 14px;
-    cursor: pointer;
+  }
 
-    @media (max-width: 600px) {
-      height: 55px;
+  @media (max-width: 840px) {
+    width: 45%;
+    padding: 0 2.5%;
+    h1 {
+      font-size: 86px;
+      line-height: 92px;
+    }
+
+    h2 {
+      font-size: 32px;
+      line-height: 42px;
     }
   }
 
   @media (max-width: 600px) {
-    height: 100vh;
-    flex-direction: initial;
-    padding-top: 40px;
+    width: 100%;
+    flex-grow: 2;
+    padding: 20px 10%;
+    form {
+      a {
+        font-size: 16px;
+        line-height: 20px;
+      }
+    }
 
-    padding-left: 0;
-    padding-right: 0;
+    input {
+      height: 48px;
+      margin-bottom: 15px;
+    }
+
+    button {
+      height: 48px;
+      font-size: 24px;
+      line-height: 32px;
+      margin-bottom: 15px;
+    }
   }
 `;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -43,83 +43,83 @@ export default function SignUp(props) {
   }
 
   return (
-    <>
-      <Container>
-        <SideLeft>
-          <h1>Linkr</h1>
-          <h2>
-            save, share and discover<br></br>
-            the best links on the web
-          </h2>
-        </SideLeft>
+    <Container>
+      <SideLeft>
+        <h1>Linkr</h1>
+        <h2>
+          save, share and discover<br></br>
+          the best links on the web
+        </h2>
+      </SideLeft>
 
-        <SideRight>
-          <form onSubmit={handleSubmit}>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              disabled={isLoading}
-              placeholder="e-mail"
-              name="email"
-              type="email"
-              required
-            ></input>
+      <SideRight>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            disabled={isLoading}
+            placeholder="e-mail"
+            name="email"
+            type="email"
+            required
+          ></input>
 
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              disabled={isLoading}
-              placeholder="password"
-              name="password"
-              type="password"
-              required
-            ></input>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            disabled={isLoading}
+            placeholder="password"
+            name="password"
+            type="password"
+            required
+          ></input>
 
-            <input
-              onChange={(e) => setUserName(e.target.value)}
-              value={username}
-              disabled={isLoading}
-              placeholder="username"
-              name="username"
-              type="text"
-              required
-            ></input>
+          <input
+            onChange={(e) => setUserName(e.target.value)}
+            value={username}
+            disabled={isLoading}
+            placeholder="username"
+            name="username"
+            type="text"
+            required
+          ></input>
 
-            <input
-              onChange={(e) => setPictureUrl(e.target.value)}
-              value={pictureUrl}
-              disabled={isLoading}
-              placeholder="picture url"
-              name="picture"
-              type="text"
-              required
-            ></input>
+          <input
+            onChange={(e) => setPictureUrl(e.target.value)}
+            value={pictureUrl}
+            disabled={isLoading}
+            placeholder="picture url"
+            name="picture"
+            type="text"
+            required
+          ></input>
 
-            <button type="submit" disabled={isLoading}>
-              {" "}
-              Sign Up{" "}
-            </button>
-            <Link to="/">
-              <p>Switch back to log in</p>
-            </Link>
-          </form>
-        </SideRight>
-      </Container>
-    </>
+          <button type="submit" disabled={isLoading}>
+            {" "}
+            Sign Up{" "}
+          </button>
+          <Link to="/">
+            <p>Switch back to log in</p>
+          </Link>
+        </form>
+      </SideRight>
+    </Container>
   );
 }
+
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
+  width: 100vw;
   height: 100vh;
 
   @media (max-width: 600px) {
-    display: initial;
-    height: 100%;
+    flex-direction: column;
   }
 `;
 const SideLeft = styled.div`
-  width: 100%;
+  width: 60%;
+  height: 100%;
   background-color: #151515;
 
   color: white;
@@ -128,7 +128,8 @@ const SideLeft = styled.div`
   flex-direction: column;
   justify-content: center;
 
-  padding-left: 144px;
+  padding-left: 5%;
+  padding-right: 15%;
 
   h1 {
     font-size: 106px;
@@ -137,40 +138,71 @@ const SideLeft = styled.div`
   }
 
   h2 {
-    font-family: "Oswald", sans-serif;
+    font-family: "Oswald";
     font-weight: 700;
     font-size: 43px;
     line-height: 64px;
   }
+
+  @media (max-width: 840px) {
+    width: 55%;
+    padding-left: 4%;
+    padding-right: 12%;
+    h1 {
+      font-size: 86px;
+      line-height: 92px;
+    }
+
+    h2 {
+      font-size: 32px;
+      line-height: 42px;
+    }
+  }
+
   @media (max-width: 600px) {
-    height: 175px;
-    align-items: center;
-    padding-left: 0;
+    width: 100%;
+    height: 40%;
+    text-align: center;
+    padding: 20px 10px;
     h1 {
       font-size: 76px;
       line-height: 84px;
     }
 
     h2 {
-      font-size: 23px;
-      line-height: 34px;
+      font-size: 22px;
+      line-height: 30px;
+    }
+  }
+  @media (max-width: 360px) {
+    height: 35%;
+    h1 {
+      font-size: 54px;
+      line-height: 60px;
+    }
+
+    h2 {
+      font-size: 18px;
+      line-height: 20px;
     }
   }
 `;
 const SideRight = styled.div`
+  width: 40%;
+  height: 100%;
   background-color: #333333;
 
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  align-items: center;
 
-  padding-left: 52px;
-  padding-right: 54px;
+  padding: 0 5%;
 
   form {
     display: flex;
     flex-direction: column;
-    p {
+    align-items: center;
+    width: 100%;
+    a {
       color: white;
       font-family: "Lato", sans-serif;
       font-style: normal;
@@ -178,26 +210,23 @@ const SideRight = styled.div`
       font-size: 20px;
       line-height: 24px;
       text-decoration-line: underline;
-      margin: 0 auto;
+      text-align: center;
     }
   }
 
   input {
-    width: 429px;
+    width: 100%;
     height: 65px;
     border-radius: 6px;
     border: none;
     margin-bottom: 13px;
-    @media (max-width: 600px) {
-      height: 55px;
-    }
   }
 
   button {
     color: white;
-    width: 433px;
+    width: 100%;
     height: 65px;
-    font-family: "Oswald";
+    font-family: "Oswald", sans-serif;
     font-weight: 700;
     font-size: 27px;
     line-height: 40px;
@@ -205,18 +234,43 @@ const SideRight = styled.div`
     border-radius: 6px;
     border: none;
     margin-bottom: 14px;
-    cursor: pointer;
+  }
 
-    @media (max-width: 600px) {
-      height: 55px;
+  @media (max-width: 840px) {
+    width: 45%;
+    padding: 0 2.5%;
+    h1 {
+      font-size: 86px;
+      line-height: 92px;
+    }
+
+    h2 {
+      font-size: 32px;
+      line-height: 42px;
     }
   }
-  @media (max-width: 600px) {
-    height: 100vh;
-    flex-direction: initial;
-    padding-top: 40px;
 
-    padding-left: 0;
-    padding-right: 0;
+  @media (max-width: 600px) {
+    width: 100%;
+    flex-grow: 2;
+    padding: 20px 10%;
+    form {
+      a {
+        font-size: 16px;
+        line-height: 20px;
+      }
+    }
+
+    input {
+      height: 48px;
+      margin-bottom: 15px;
+    }
+
+    button {
+      height: 48px;
+      font-size: 24px;
+      line-height: 32px;
+      margin-bottom: 15px;
+    }
   }
 `;
